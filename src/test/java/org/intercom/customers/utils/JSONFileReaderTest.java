dad9585ab1e760465f547c144829dc67f4a61a88
KANIKA <kanika.ghiloria93@gmail.com>
2020-08-09 15:05:26 +0100

    first commit

---
+++ src/test/java/org/intercom/customers/utils/JSONFileReaderTest.java
@@ -0,0 +1,18 @@
+package org.intercom.customers.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JSONFileReaderTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void copyFileFromUrl() {
+    }
+}
